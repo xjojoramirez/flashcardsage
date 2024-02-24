38ca6b384d6b460c40be1b01cc8ece2afe8d798d
jojo <jojoryan.ramirez@gmail.com>
2024-02-24 21:18:10 +0800

    Adding the ability to delete decks and refactoring code for clarity

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { Link } from "react-router-dom";
+import { deleteDeck } from './api/deleteDeck';
+import { TDeck, getDecks } from './api/getDecks';
+import { createDeck } from './api/createDeck';
 
-type TDeck = {
-  title: string;
-  _id: string;
-}
+
 
 function App() {
   const [decks, setDecks] = useState<TDeck[]>([]);
@@ -12,22 +13,19 @@ function App() {
 
   async function handleCreateDeck(e: React.FormEvent){
     e.preventDefault();
-    await fetch('http://localhost:5000/decks',{
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-      }),
-      headers:{
-       "Content-Type":"application/json",
-      }
-    });
+    const deck = await createDeck(title);
+    setDecks([...decks, deck])
     setTitle("");
+  }
+
+  async function handleDeleteDeck(deckId: string) {
+    await deleteDeck(deckId);
+    setDecks(decks.filter((deck) => deck._id !== deckId));
   }
 
   useEffect(() =>{
     async function fetchDecks() {
-      const response = await fetch("http://localhost:5000/decks"); 
-      const newDecks = await response.json();
+      const newDecks = await getDecks()
       setDecks(newDecks);
     }
     fetchDecks();
@@ -38,7 +36,10 @@ function App() {
         <ul className="decks">
           {
             decks.map((deck) => (
-              <li key={deck._id}>{deck.title}</li>
+              <li key={deck._id}>
+                <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
+                <Link to={`decks/${deck._id}`}>{deck.title}</Link>
+              </li>
             ))
           }
         </ul>
--- server/src/index.ts
+++ server/src/index.ts
@@ -23,17 +23,19 @@ app.get('/decks', async (req: Request, res: Response) => {
     res.json(decks);
 })
 
-
-app.get('/',(req: Request, res: Response) => {
-    res.send('index page');
-})
-
 app.post('/decks', async (req: Request, res: Response) => {
     const newDeck = new Deck({
         title:req.body.title,
     });
     const createdDeck = await newDeck.save();
     res.json(createdDeck);
+})
+
+app.delete('/decks/:deckId', async (req: Request, res: Response) => {
+    const deckId = req.params.deckId;
+    const deck = await Deck.findByIdAndDelete(deckId);
+    res.json(deck);
+    
 })
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
